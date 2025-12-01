e6f390b1b8326bff5a480e00d1d2ed71fa317de1
Fax79 <fabio.terrasi@gmail.com>
2025-12-01 22:01:40 +0100

    Update Guida_2.0.py

--- Guida_2.0.py
+++ Guida_2.0.py
@@ -429,7 +429,7 @@ with st.container():
                     2. Se devi fare un confronto, usa elenchi puntati descrittivi.
                     3. Usa ESATTAMENTE la struttura seguente.
                     4. Scrivi paragrafi ricchi e lunghi.
-                    5. NON USARE MAI CARATTERI SPECIALI, simboli delle valute (come Euro o Dollaro), semplifica la grafia delle parole straniere utilizzando l'alfabeto standard, ammesse SOLO lettere accentate comunemente usate in italiano.
+                    5. NON USARE MAI CARATTERI SPECIALI, NON USARE simboli delle valute (come Euro o Dollaro), semplifica SEMPRE la grafia delle parole straniere INCLUSI I NOMI DEI LUOGHI, utilizzando SOLO l'alfabeto italiano standard, ammesse SOLO lettere accentate comunemente usate in italiano.
                     6. Se viene inserita un parola che non è una città o una frase rispondi in modo scherzoso.
                     
                     MODELLO:
@@ -537,3 +537,4 @@ st.markdown("""
     </p>
 </div>
 """, unsafe_allow_html=True)
+
