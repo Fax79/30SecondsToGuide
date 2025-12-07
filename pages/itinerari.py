957d79c3ab565ff4ce21b63026522075cb9fdbfa
Fax79 <fabio.terrasi@gmail.com>
2025-12-07 03:41:04 +0100

    Update itinerari.py

--- pages/itinerari.py
+++ pages/itinerari.py
@@ -206,14 +206,14 @@ def create_complex_pdf(text, destination, meta_data):
         
         # --- LOGICA BLINDATA LINK ---
         if "## CAPITOLO 2" in line_upper and not inserted_ch1:
-            banner_text = f"I biglietti dei voli in {month_clean} aumentano? Blocca le tariffe migliori su Kiwi.com"
+            banner_text = f"In {month_clean} i prezzi aumentano? Prenota ora su Kiwi.com"
             make_box(pdf, banner_text, FLIGHT_LINK, "green")
             make_box(pdf, "eSim Saily: Internet immediato all'arrivo senza acquisto di SIM locali", ESIM_LINK, "yellow")
             make_box(pdf, "MAI senza Assicurazione Sanitaria: Approfitta QUI dello sconto 10% con Heymondo", INSURANCE_LINK, "green")
             inserted_ch1 = True
             
         elif "## CAPITOLO 3" in line_upper and not inserted_ch2:
-            make_box(pdf, f"Le stanze in Hotel si esauriscono velocemente in {month_clean}, verifica disponibilità e prezzi su Expedia", HOTEL_LINK, "blue")
+            make_box(pdf, f"Stanze in Hotel quasi esaurite in {month_clean}? Prenota ora su Expedia", HOTEL_LINK, "blue")
             make_box(pdf, "Transfer privati ad un prezzo WOW! da e per l'aeroporto", TRANSF_LINK, "purple")
             inserted_ch2 = True
 
