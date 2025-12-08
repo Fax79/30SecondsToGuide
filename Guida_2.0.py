7b4b9a28caf91924526ed5260486442030aa3779
Fax79 <fabio.terrasi@gmail.com>
2025-12-08 10:55:07 +0100

    Update Guida_2.0.py

--- Guida_2.0.py
+++ Guida_2.0.py
@@ -614,7 +614,8 @@ with st.container():
                     3. Usa ESATTAMENTE la struttura seguente.
                     4. Scrivi paragrafi ricchi e lunghi.
                     5. NON USARE MAI CARATTERI SPECIALI, simboli delle valute (come Euro o Dollaro), semplifica la grafia delle parole straniere utilizzando l'alfabeto standard, ammesse SOLO lettere accentate comunemente usate in italiano.
-                    6. Se viene inserita un parola che non è una città o una frase rispondi in modo scherzoso ma sintetico, non usare la struttura della guida.
+                    6. Se viene inserita una nazione, una regione, un'area geografica produci la guida per la città principale, aggiungi una premessa prima del capitolo 1 in cui elenchi eventuali altre città esortando a fare guide separate, suggerisci anche di utilizzare il bottone dell'"ITINERARY WIZARD" che trovano nel sito.
+                    7. Se viene inserita un parola o una frase che non sono luoghi geografici rispondi in modo scherzoso ma sintetico, non usare la struttura della guida.
                     
                     MODELLO:
                     {TESTO_MODELLO}
@@ -722,3 +723,4 @@ st.markdown("""
     </p>
 </div>
 """, unsafe_allow_html=True)
+
